653674565f8f05f332b7590c8923bf015a156f75
Jean-Christophe-VERMI <56582665+Jean-Christophe-VERMI@users.noreply.github.com>
2020-04-24 14:22:07 +0200

    initial config react + node express

---
+++ client/globalapp/src/App.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Page d'accueil qui réunit l'ensemble des projets développés
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Voir les projets
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
