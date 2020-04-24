f21192b97c79242c051508858980afce132f6541
Jean-Christophe-VERMI <56582665+Jean-Christophe-VERMI@users.noreply.github.com>
2020-04-24 14:43:44 +0200

    ajout du dossier build

---
+++ client/globalapp/build/precache-manifest.8012814a9075af66bbba343fea3678a2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d21a39e968aa6baab793e9437d778774",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f5ed4ff855b1f5c7bc1a",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "864201f29521b294ad71",
+    "url": "/static/js/2.fb1358de.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.fb1358de.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f5ed4ff855b1f5c7bc1a",
+    "url": "/static/js/main.d9e4be1a.chunk.js"
+  },
+  {
+    "revision": "662c92672839f96bdf3c",
+    "url": "/static/js/runtime-main.f7614a76.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
