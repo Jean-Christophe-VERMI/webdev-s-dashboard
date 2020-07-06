1840f92690c40e3eb74fcc6419ff4df6bce44a53
Jean-Christophe-VERMI <56582665+Jean-Christophe-VERMI@users.noreply.github.com>
2020-07-06 09:49:57 +0200

    mise en place d'une configuration d'authentification par jwt

--- app/controllers/userController.js
+++ app/controllers/userController.js
@@ -89,13 +89,13 @@ const userController = {
           Vous pouvez maintenant vérifier votre e-mail pour valider votre inscription. 
           <br/>
           Veuillez copier votre code de validation : <b>${secretToken}</b> et renseigez le à l'adresse suivante :
-          <a href="http://localhost:3000/verification-email">http://localhost:3000/verification-email</a>
+          <a href="http://localhost:3000/verification-email?secretToken=${secretToken}">http://localhost:3000/verification-email</a>
           <br/>
           Rappel de vos identifiants de connexion :
           <br/>
           Identifiant : ${email}
           <br/>
-          Mot de passe : ${password}
+          
           <br/>
           A bientôt !
           `;
